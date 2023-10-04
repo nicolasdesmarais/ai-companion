f99484e074454def465a3c61ec14a5f974f69ecc
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-04 13:10:08 -0600

    Move workspace POST API under /me/workspaces

--- app/api/v1/me/workspaces/route.ts
+++ app/api/v1/me/workspaces/route.ts
@@ -18,3 +18,23 @@ export async function GET(req: Request) {
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { name } = body;
+
+    if (!user || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const workspaceService = new WorkspaceService();
+    const workspace = workspaceService.createWorkspace(user.id, name);
+
+    return NextResponse.json(workspace);
+  } catch (error) {
+    console.log("[WORKSPACE_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
