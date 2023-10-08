9f1430706f25176061c3dec844b385cb29a12e69
Dmitry Torba <dmitrytorba@gmail.com>
2023-10-08 09:09:22 -0700

    feat(login) start new design

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,20 @@ const AuthLayout = ({
     children: React.ReactNode;
   }) => {
     return ( 
-      <div className="flex justify-center items-center h-full">
-        {children}
+      <div className="blue-bg h-full">
+        <div className="flex justify-center items-center">
+          <h1 className="mt-5 text-2xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-4xl dark:text-white">AppDirect AI</h1>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <div className="auth-side-panel">
+            <h2 className="me-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Let AI change the way you work.</h2>
+            <div>
+              AppDirect gives you the ability to access AIs that help you be more successful in work and life.
+              You can browse our catalog of community made AIs or create your own!
+            </div>
+          </div>
+          {children}
+        </div>
       </div>
      );
   }
