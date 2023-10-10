3521fe287ccbf932a9fd892f39d73c24529f8ef9
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-10 14:52:44 -0600

    Change "Newest" to "All" for categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import qs from "query-string";
-import { Category } from "@prisma/client"
+import { Category } from "@prisma/client";
 import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 import { cn } from "@/lib/utils";
 
 interface CategoriesProps {
-  data: Category[]
+  data: Category[];
 }
 
-export const Categories = ({
-  data
-}: CategoriesProps) => {
+export const Categories = ({ data }: CategoriesProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -21,10 +19,13 @@ export const Categories = ({
   const onClick = (id: string | undefined) => {
     const query = { categoryId: id };
 
-    const url = qs.stringifyUrl({
-      url: window.location.href,
-      query
-    }, { skipNull: true });
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
 
     router.push(url);
   };
@@ -33,45 +34,47 @@ export const Categories = ({
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
         onClick={() => onClick(undefined)}
-        className={cn(`
-          flex 
-          items-center 
-          text-center 
-          text-xs 
-          md:text-sm 
-          px-2 
-          md:px-4 
-          py-2 
-          md:py-3 
-          rounded-md 
-          bg-primary/10 
-          hover:opacity-75 
+        className={cn(
+          `
+          flex
+          items-center
+          text-center
+          text-xs
+          md:text-sm
+          px-2
+          md:px-4
+          py-2
+          md:py-3
+          rounded-md
+          bg-primary/10
+          hover:opacity-75
           transition
         `,
-          !categoryId ? 'bg-accent' : 'bg-primary/10'
+          !categoryId ? "bg-accent" : "bg-primary/10"
         )}
       >
-        Newest
+        All
       </button>
       {data.map((item) => (
         <button
           onClick={() => onClick(item.id)}
-          className={cn(`
-            flex 
-            items-center 
-            text-center 
-            text-xs 
-            md:text-sm 
-            px-2 
-            md:px-4 
-            py-2 
-            md:py-3 
-            rounded-md 
-            bg-primary/10 
-            hover:opacity-75 
+          className={cn(
+            `
+            flex
+            items-center
+            text-center
+            text-xs
+            md:text-sm
+            px-2
+            md:px-4
+            py-2
+            md:py-3
+            rounded-md
+            bg-primary/10
+            hover:opacity-75
             transition
           `,
-            item.id === categoryId ? 'bg-accent' : 'bg-primary/10'
+            item.id === categoryId ? "bg-accent" : "bg-primary/10"
           )}
           key={item.id}
         >
@@ -79,5 +82,5 @@ export const Categories = ({
         </button>
       ))}
     </div>
-  )
-}
+  );
+};
