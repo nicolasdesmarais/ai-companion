0db1292a2ca2d65dd73d10f4c9b78c5949aca1f8
Dmitry Torba <dmitrytorba@gmail.com>
2023-10-09 19:00:47 -0700

    feat(nav) new style side nav

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,9 @@
 
 import { Home, Plus, Settings, Wrench } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
-
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import { useProModal } from "@/hooks/use-pro-modal";
 
@@ -10,9 +12,7 @@ interface SidebarProps {
   isPro: boolean;
 }
 
-export const Sidebar = ({
-  isPro
-}: SidebarProps) => {
+export const Sidebar = ({ isPro }: SidebarProps) => {
   const proModal = useProModal();
   const router = useRouter();
   const pathname = usePathname();
@@ -23,56 +23,68 @@ export const Sidebar = ({
     }
 
     return router.push(url);
-  }
+  };
 
   const routes = [
     {
       icon: Home,
-      href: '/',
+      href: "/",
       label: "Home",
       pro: false,
     },
     {
       icon: Plus,
-      href: '/companion/new',
+      href: "/companion/new",
       label: "Create",
       pro: false,
     },
     {
       icon: Wrench,
-      href: '/dashboard',
+      href: "/dashboard",
       label: "Tools",
-      pro: false,
+      pro: true,
     },
     {
       icon: Settings,
-      href: '/settings',
+      href: "/settings",
       label: "Settings",
-      pro: false,
+      pro: true,
     },
-    
   ];
 
   return (
-    <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-      <div className="p-3 flex-1 flex justify-center">
-        <div className="space-y-2">
-          {routes.map((route) => (
-            <div
-              onClick={() => onNavigate(route.href, route.pro)}
-              key={route.href}
-              className={cn(
-                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
-                pathname === route.href && "bg-accent text-primary",
-              )}
-            >
-              <div className="flex flex-col gap-y-2 items-center flex-1">
-                <route.icon className="h-5 w-5" />
-                {route.label}
-              </div>
+    <div className="p-3 flex-1 flex justify-between flex-col h-full">
+      <div className="space-y-2 flex flex-col items-center">
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+          }}
+        />
+        {routes.map((route) => (
+          <div
+            onClick={() => onNavigate(route.href, route.pro)}
+            key={route.href}
+            className={cn(
+              "text-muted-foreground text-xs group py-3 px-8 flex w-full justify-center font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+              pathname === route.href && "bg-accent text-primary",
+              route.pro && "hidden"
+            )}
+          >
+            <div className="flex flex-col items-center flex-1">
+              <route.icon className="h-5 w-5 mb-1" />
+              {route.label}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
+      <div className="space-y-2 flex flex-col items-center py-3 px-8">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            baseTheme: dark,
+          }}
+        />
       </div>
     </div>
   );
