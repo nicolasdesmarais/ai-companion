58da1605986d2e29b70ab408679c540579ca7362
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-14 15:42:08 -0600

    Change NextApiRequest to NextRequest

--- app/api/v1/integrations/google-drive/auth/route.ts
+++ app/api/v1/integrations/google-drive/auth/route.ts
@@ -1,6 +1,6 @@
 import { google } from "googleapis";
-import { NextApiRequest, NextApiResponse } from "next";
 import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
 const OAUTH2_CLIENT = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
@@ -10,7 +10,7 @@ const OAUTH2_CLIENT = new google.auth.OAuth2(
 
 const SCOPE = ["https://www.googleapis.com/auth/drive.readonly"];
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const url = OAUTH2_CLIENT.generateAuthUrl({
     access_type: "offline",
     scope: SCOPE,
