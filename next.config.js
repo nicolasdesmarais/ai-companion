b848d26aff3289f0357e383c207cf148720df55f
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-17 08:43:01 -0600

    Add headers to allow CORS

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        "res.cloudinary.com",
-        "oaidalleapiprodscus.blob.core.windows.net"
-      ],
-    },
-  };  
-  
-module.exports = nextConfig
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "oaidalleapiprodscus.blob.core.windows.net",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Cookie",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
