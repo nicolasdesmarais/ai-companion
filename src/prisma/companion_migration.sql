9802a495e6c27ed669bd93cf906114ed517dc451
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-27 17:01:35 -0600

    Remove migration files

--- src/prisma/companion_migration.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ais` (`id`, `org_id`, `user_id`, `user_name`, `src`, `name`, `description`, `instructions`, `seed`, `visibility`, `options`, `created_at`, `updated_at`, `category_id`, `model_id`)
-select
-`id`, `org_id`, `userId`, `userName`, `src`, `name`, `description`, `instructions`, `seed`, `visibility`, `options`, `created_at`, `updated_at`, `categoryId`, `modelId` From Companion;
