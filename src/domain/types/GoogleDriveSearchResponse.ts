b0f5d193a114d16dc957f8744b112e8000640ad1
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-30 15:56:15 -0600

    Update google drive file labels

--- src/domain/types/GoogleDriveSearchResponse.ts
+++ src/domain/types/GoogleDriveSearchResponse.ts
@@ -24,6 +24,12 @@ export function mapMimeTypeToEnum(
       return FileType.PDF;
     case FileType.DOCX:
       return FileType.DOCX;
+    case FileType.GOOGLE_DOC:
+      return FileType.GOOGLE_DOC;
+    case FileType.GOOGLE_SHEETS:
+      return FileType.GOOGLE_SHEETS;
+    case FileType.GOOGLE_SLIDES:
+      return FileType.GOOGLE_SLIDES;
     case FileType.FOLDER:
       return FileType.FOLDER;
     default:
@@ -43,6 +49,12 @@ export function getLabelFromFileType(fileType: FileType) {
       return "PDF";
     case FileType.DOCX:
       return "Document";
+    case FileType.GOOGLE_DOC:
+      return "Google Document";
+    case FileType.GOOGLE_SHEETS:
+      return "Google Sheet";
+    case FileType.GOOGLE_SLIDES:
+      return "Google Slides";
     case FileType.FOLDER:
       return "Folder";
     default:
@@ -56,6 +68,9 @@ enum FileType {
   EPUB = "application/epub+zip",
   PDF = "application/pdf",
   DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  GOOGLE_DOC = "application/vnd.google-apps.document",
+  GOOGLE_SHEETS = "application/vnd.google-apps.spreadsheet",
+  GOOGLE_SLIDES = "application/vnd.google-apps.presentation",
   FOLDER = "application/vnd.google-apps.folder",
   UNKNOWN = "unknown",
 }
