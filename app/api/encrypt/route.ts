4f0d30af8be632ed7af43b27a2bcb81752ee3b75
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-29 19:35:12 -0600

    Remove test route

--- app/api/encrypt/route.ts
+++
@@ -1,9 +0,0 @@
-import { decrypt, encrypt } from "@/src/lib/encryptionUtils";
-
-export async function GET(req: Request) {
-  const encryptedData = encrypt("test");
-  console.log("encryptedData", encryptedData);
-
-  const decryptedData = decrypt(encryptedData);
-  console.log("decryptedData", decryptedData);
-}
