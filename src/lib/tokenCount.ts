36b196e33aeac3e6d5b332ba1fbe885417ddbcb8
Dmitry Torba <dmitrytorba@gmail.com>
2023-10-30 17:26:09 -0700

    fix(chat) switch tiktoken from wasm to js

--- src/lib/tokenCount.ts
+++ src/lib/tokenCount.ts
@@ -1,12 +1,5 @@
-import { Tiktoken } from "tiktoken/lite";
-import cl100k_base from "tiktoken/encoders/cl100k_base.json";
+import { getEncoding } from "js-tiktoken";
 
-const encoding = new Tiktoken(
-  cl100k_base.bpe_ranks,
-  cl100k_base.special_tokens,
-  cl100k_base.pat_str
-);
+const encoding = getEncoding("cl100k_base");
 
-export const getEncoding = (value: string) => encoding.encode(value);
-
-export const getTokenLength = (value: string) => getEncoding(value).length;
+export const getTokenLength = (value: string) => encoding.encode(value).length;
