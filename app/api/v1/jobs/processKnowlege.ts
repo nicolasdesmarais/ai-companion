5b8efd6237e0f572a463045c1d1d6c7d1624d35c
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-10-31 22:44:45 -0600

    Update cron function

--- app/api/v1/jobs/processKnowlege.ts
+++ app/api/v1/jobs/processKnowlege.ts
@@ -1,10 +1,8 @@
-import { NextRequest } from "next/server";
-
 export const config = {
   runtime: "edge",
 };
 
-export default async function handler(req: NextRequest) {
+export async function POST(req: Request) {
   console.log("CRON request received");
   console.log("req:", req);
 }
