50776073e60e7d4e0447e6ddd800fca5d5aab3ea
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-11-01 13:29:50 -0600

    Log request for cron function

--- app/api/v1/cron/route.ts
+++ app/api/v1/cron/route.ts
@@ -2,8 +2,10 @@ import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-export async function GET() {
+export async function GET(req: Request) {
   console.log("Cron function");
+  console.log("Req:" + JSON.stringify(req));
+  console.log("Headers:" + JSON.stringify(req.headers));
 
   return NextResponse.json({ message: "Cron function" });
 }
