2c58f9bc2d4bf34ef84388cbc00a7274365f91d9
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-11-02 13:12:16 -0600

    Add dataSourcePersisted function

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,14 @@
 import { apifyWebhookReceived } from "@/src/adapters/inngest/apify/apify-webhooks";
+import { dataSourcePersisted } from "@/src/adapters/inngest/datasources/datasource-functions";
 import { googleDriveDataSourceCreationRequested } from "@/src/adapters/inngest/google-drive/google-drive-functions";
 import { serve } from "inngest/next";
 import { inngest } from "../../../src/adapters/inngest/client";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [apifyWebhookReceived, googleDriveDataSourceCreationRequested],
+  functions: [
+    apifyWebhookReceived,
+    dataSourcePersisted,
+    googleDriveDataSourceCreationRequested,
+  ],
 });
