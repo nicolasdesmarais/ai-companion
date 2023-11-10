34cc293d65752a9d8c44ee4d8aaf01b86bb44e21
Dmitry Torba <dmitrytorba@gmail.com>
2023-11-10 10:52:13 -0800

    feat(knowledge) more superusers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,10 @@ export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
 }
 
-const superusers = ["user_2WXwDNSh5x5zxnabNzpSN2Z1JWs"];
+const superusers = [
+  "user_2WXwDNSh5x5zxnabNzpSN2Z1JWs",
+  "user_2Wr3B1xXEqC2rZaa9uvzG61sFys",
+];
 export function isSuperuser(userId: string) {
   return superusers.includes(userId);
 }
