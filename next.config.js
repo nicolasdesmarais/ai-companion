bb40511d29bdab03476541dd0011c7e5287bd2ca
Dmitry Torba <dmitrytorba@gmail.com>
2023-11-15 09:53:11 -0800

    chore(logs) ignore import errors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
       "oaidalleapiprodscus.blob.core.windows.net",
     ],
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /original-fs/,
+        contextRegExp: /adm-zip/,
+      })
+    );
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /zipfile/,
+        contextRegExp: /epub2/,
+      })
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
