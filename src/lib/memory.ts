94912e807063fc839b585e16c165e6d5fae265c5
Dmitry Torba <dmitrytorba@gmail.com>
2023-11-17 11:00:15 -0800

    fix(ai) lower embedding concurrency more

--- src/lib/memory.ts
+++ src/lib/memory.ts
@@ -10,7 +10,7 @@ const embeddingsConfig = {
   azureOpenAIApiInstanceName: "prod-appdirectai-east2",
   azureOpenAIApiDeploymentName: "text-embedding-ada-002",
   batchSize: 16,
-  maxConcurrency: 100,
+  maxConcurrency: 40,
 };
 
 export type AIKey = {
