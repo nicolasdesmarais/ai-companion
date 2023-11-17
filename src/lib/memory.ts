2738e8e14ebd333684a3eadb1655c3334e21f87c
Dmitry Torba <dmitrytorba@gmail.com>
2023-11-17 12:17:40 -0800

    fix(ai) no embedding concurrency

--- src/lib/memory.ts
+++ src/lib/memory.ts
@@ -10,7 +10,7 @@ const embeddingsConfig = {
   azureOpenAIApiInstanceName: "prod-appdirectai-east2",
   azureOpenAIApiDeploymentName: "text-embedding-ada-002",
   batchSize: 16,
-  maxConcurrency: 4,
+  maxConcurrency: 1,
 };
 
 export type AIKey = {
