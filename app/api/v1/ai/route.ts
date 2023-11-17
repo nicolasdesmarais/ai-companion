64709af06d53b609e974ad0a939bf51c1d378549
Dmitry Torba <dmitrytorba@gmail.com>
2023-11-17 09:48:01 -0800

    fix(ai) seed crash

--- app/api/v1/ai/route.ts
+++ app/api/v1/ai/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
         name,
         description,
         instructions,
-        seed,
+        seed: seed ?? "",
         modelId,
         visibility,
         options,
