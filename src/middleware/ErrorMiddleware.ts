d86da118a0d57906760c0759c4f42004392de6a6
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-11-21 20:20:30 +0200

    Refactor ErrorMiddleware

--- src/middleware/ErrorMiddleware.ts
+++ src/middleware/ErrorMiddleware.ts
@@ -2,36 +2,35 @@ import { ApiError } from "@/src/ports/api/ApiError";
 import { NextResponse } from "next/server";
 import { EntityNotFoundError, ForbiddenError } from "../domain/errors/Errors";
 
-// This function takes a handler and returns a new handler that includes the try-catch logic
-export const withErrorHandler = (handler: Function) => {
+type Handler = (...args: any[]) => Promise<any>;
+
+const errorStatusMap = new Map<Function, number>([
+  [EntityNotFoundError, 404],
+  [ForbiddenError, 403],
+]);
+
+const createApiErrorResponse = (message: string, status: number) => {
+  const apiError: ApiError = { message };
+  return new NextResponse(JSON.stringify(apiError), {
+    status,
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const withErrorHandler = (handler: Handler) => {
   return async (...args: any[]) => {
     try {
       // Call the original handler
       return await handler(...args);
     } catch (error) {
-      let apiError: ApiError;
-
-      // Handle known error types
-      if (error instanceof EntityNotFoundError) {
-        apiError = {
-          message: error.message,
-        };
-        return new NextResponse(JSON.stringify(apiError), {
-          status: 404,
-        });
+      // Check if error type is known and has a mapped status code
+      const status = errorStatusMap.get(error.constructor);
+      if (status) {
+        return createApiErrorResponse(error.message, status);
       }
-      if (error instanceof ForbiddenError) {
-        apiError = {
-          message: error.message,
-        };
 
-        return new NextResponse(JSON.stringify(apiError), { status: 403 });
-      }
       // Handle generic errors
-      apiError = {
-        message: "Internal Error",
-      };
-      return new NextResponse(JSON.stringify(apiError), { status: 500 });
+      return createApiErrorResponse("Internal Server Error", 500);
     }
   };
 };
