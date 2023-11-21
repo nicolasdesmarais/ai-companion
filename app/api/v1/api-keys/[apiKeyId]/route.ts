3dad06bfcba919b5c96443fa5ddcbe56d2d60849
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-11-21 20:11:10 +0200

    Error handling for keys API

--- app/api/v1/api-keys/[apiKeyId]/route.ts
+++ app/api/v1/api-keys/[apiKeyId]/route.ts
@@ -1,62 +1,45 @@
-import {
-  EntityNotFoundError,
-  ForbiddenError,
-} from "@/src/domain/errors/Errors";
 import apiKeyService from "@/src/domain/services/ApiKeyService";
+import { withErrorHandler } from "@/src/middleware/ErrorMiddleware";
 import { UpdateApiKeyRequest } from "@/src/ports/api/ApiKeysApi";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
+export async function putHandler(
   request: Request,
   { params }: { params: { apiKeyId: string } }
 ) {
-  try {
-    const { userId, orgId } = auth();
+  const { userId, orgId } = auth();
 
-    if (!userId || !orgId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    await apiKeyService.deleteApiKey(orgId, userId, params.apiKeyId);
-
-    return new NextResponse(null, { status: 204 });
-  } catch (error) {
-    console.log("[API KEY DELETE]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+  if (!userId || !orgId) {
+    return new NextResponse("Unauthorized", { status: 401 });
   }
+
+  const body: UpdateApiKeyRequest = await request.json();
+
+  const updatedKey = await apiKeyService.updateApiKey(
+    orgId,
+    userId,
+    params.apiKeyId,
+    body
+  );
+
+  return NextResponse.json(updatedKey);
 }
 
-export async function PUT(
+export async function deleteHandler(
   request: Request,
   { params }: { params: { apiKeyId: string } }
 ) {
-  try {
-    const { userId, orgId } = auth();
+  const { userId, orgId } = auth();
 
-    if (!userId || !orgId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const body: UpdateApiKeyRequest = await request.json();
-
-    const updatedKey = await apiKeyService.updateApiKey(
-      orgId,
-      userId,
-      params.apiKeyId,
-      body
-    );
-
-    return NextResponse.json(updatedKey);
-  } catch (error) {
-    if (error instanceof EntityNotFoundError) {
-      return new NextResponse("Not Found", { status: 404 });
-    }
-    if (error instanceof ForbiddenError) {
-      return new NextResponse("Forbidden", { status: 403 });
-    }
-
-    console.log("[API KEY UPDATE]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+  if (!userId || !orgId) {
+    return new NextResponse("Unauthorized", { status: 401 });
   }
+
+  await apiKeyService.deleteApiKey(orgId, userId, params.apiKeyId);
+
+  return new NextResponse(null, { status: 204 });
 }
+
+export const PUT = withErrorHandler(putHandler);
+export const DELETE = withErrorHandler(deleteHandler);
