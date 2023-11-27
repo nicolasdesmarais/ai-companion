5712f8a74d62fc9d7588fb8f799d5424eaec6904
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-11-27 11:48:55 +0200

    Revert ai-models change

--- components/ai-models.ts
+++ components/ai-models.ts
@@ -31,43 +31,6 @@ export const models = [
   //   },
   // },
   {
-    id: "gpt-4-assistant",
-    name: "GPT-4 (32K Context) w/ Assistant",
-    contextSize: 32768,
-    options: {
-      temperature: {
-        default: 1,
-        max: 2,
-        min: 0,
-        step: 0.1,
-      },
-      topP: {
-        default: 1,
-        max: 1,
-        min: 0,
-        step: 0.01,
-      },
-      maxTokens: {
-        default: 4000,
-        max: 6000,
-        min: 100,
-        step: 1,
-      },
-      frequencyPenalty: {
-        default: 0,
-        max: 1,
-        min: -1,
-        step: 0.1,
-      },
-      presencePenalty: {
-        default: 0,
-        max: 1,
-        min: -1,
-        step: 0.1,
-      },
-    },
-  },
-  {
     id: "gpt-4",
     name: "GPT-4 (32K Context)",
     contextSize: 32768,
