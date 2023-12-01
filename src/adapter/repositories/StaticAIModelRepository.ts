f77274d3223c9ed58310f628f9ef693a9ce43608
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-12-01 16:50:05 +0100

    Make assistant API model visible

--- src/adapter/repositories/StaticAIModelRepository.ts
+++ src/adapter/repositories/StaticAIModelRepository.ts
@@ -83,10 +83,10 @@ export class StaticAIModelRepository implements AIModelRepository {
     },
     {
       id: "gpt-4-1106-preview-assistant",
-      name: "GPT-4 Turbo (Assistant) (32K Context)",
+      name: "GPT-4 Turbo w/ Assistant API",
       externalModelId: "gpt-4-1106-preview",
       contextSize: 32768,
-      isVisible: false,
+      isVisible: true,
       options: {
         temperature: {
           default: 1,
