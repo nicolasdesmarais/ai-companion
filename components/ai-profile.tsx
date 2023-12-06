402d8e0f23a6036efdae9ffbc7115a1dd735b1cf
Dmitry Torba <dmitrytorba@gmail.com>
2023-12-06 10:20:32 -0800

    feat(AIA-60) style profile side panel

--- components/ai-profile.tsx
+++ components/ai-profile.tsx
@@ -16,45 +16,64 @@ export const AIProfile = ({ ai, rating }: Props) => {
   const { isOpen, onClose } = useAIProfile();
   if (!isOpen) return null;
   return (
-    <div className="bg-accent/30">
+    <div className="bg-accent/30 px-6 space-y-4 w-2/3 w-full overflow-auto ml-1">
       <div className="absolute top-4 right-4">
-        <Button onClick={onClose} variant="ghost">
+        <Button onClick={onClose} variant="ghost" size="icon" type="button">
           <X className="h-6 w-6" />
         </Button>
       </div>
-      <div className="relative w-40 h-40">
-        <Image
-          fill
-          alt="AI Profile Image"
-          src={ai.src}
-          className="rounded-lg object-cover"
-        />
+      <div className="pt-1">
+        <Button variant="link" size="sm" type="button">
+          Profile
+        </Button>
+        <Button variant="link" size="sm" type="button">
+          Specifications
+        </Button>
+        <Button variant="link" size="sm" type="button">
+          Reviews
+        </Button>
       </div>
-      <div>{ai.name}</div>
-      <div>By {ai.userName}</div>
-      <div>{ai.description}</div>
-      <StarRating
-        value={Math.round(rating.averageRating)}
-        count={rating.ratingCount}
-        className=""
-      />
-      <div>{ai.profile?.headline}</div>
+      <div className="flex">
+        <div className="relative w-40 h-40 mr-4 shrink-0">
+          <Image
+            fill
+            alt="AI Profile Image"
+            src={ai.src}
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex flex-col justify-between">
+          <div>
+            <div className="text-2xl font-bold">{ai.name}</div>
+            <div className="text-sm text-muted-foreground">
+              By {ai.userName}
+            </div>
+            <div className="mt-1">{ai.description}</div>
+          </div>
+          <StarRating
+            value={Math.round(rating.averageRating)}
+            count={rating.ratingCount}
+            className=""
+          />
+        </div>
+      </div>
+      <div className="text-3xl font-bold">{ai.profile?.headline}</div>
       <div>{ai.profile?.description}</div>
-      <div>Features</div>
+      <div className="text-3xl font-bold">Features</div>
       {ai.profile?.features.map((feature: any, index: number) => (
         <div key={`feature-${index}`}>
           <div>{feature.title}</div>
           <div>{feature.description}</div>
         </div>
       ))}
-      <div>Training Specifications</div>
+      <div className="text-3xl font-bold">Training Specifications</div>
       {ai.profile?.showCharacter && <div>Character</div>}
 
       {ai.profile?.showPersonality && <div>Personality</div>}
 
       {ai.profile?.showTraining && <div>showTraining</div>}
 
-      <div>Reviews</div>
+      <div className="text-3xl font-bold">Reviews</div>
     </div>
   );
 };
