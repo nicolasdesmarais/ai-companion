f9c95f3bde689b0a00233dd5f66a98893f2b492e
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-12-08 20:14:20 +0200

    Add permissions in mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,14 +1,17 @@
 import { Menu } from "lucide-react";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Permission } from "@/src/security/models/Permission";
 
 export const MobileSidebar = ({
   isPro,
   hasChat,
+  userPermissions,
 }: {
   isPro: boolean;
   hasChat: boolean;
+  userPermissions: Permission[];
 }) => {
   return (
     <Sheet>
@@ -20,7 +23,11 @@ export const MobileSidebar = ({
         className="p-0 bg-secondary pt-10 w-32"
         id="mobile-sidebar"
       >
-        <Sidebar isPro={isPro} hasChat={hasChat} />
+        <Sidebar
+          isPro={isPro}
+          hasChat={hasChat}
+          userPermissions={userPermissions}
+        />
       </SheetContent>
     </Sheet>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Sparkles } from "lucide-react";
-import { Poppins } from "next/font/google";
 import { MobileSidebar } from "@/components/mobile-sidebar";
 import { Button } from "@/components/ui/button";
 import { useProModal } from "@/hooks/use-pro-modal";
+import { getUserAuthorizationContext } from "@/src/security/utils/securityUtils";
+import { Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
 
 const font = Poppins({ weight: "600", subsets: ["latin"] });
 interface NavbarProps {
@@ -14,10 +15,21 @@ interface NavbarProps {
 export const Navbar = ({ isPro, hasChat }: NavbarProps) => {
   const proModal = useProModal();
 
+  const authorizationContext = getUserAuthorizationContext();
+  if (!authorizationContext) {
+    return;
+  }
+
+  const { userId, permissions } = authorizationContext;
+
   return (
     <div className="fixed w-full z-30 flex justify-between items-center py-2 px-4 h-16 border-primary/10 bg-secondary md:hidden">
       <div className="flex items-center">
-        <MobileSidebar isPro={isPro} hasChat={hasChat} />
+        <MobileSidebar
+          isPro={isPro}
+          hasChat={hasChat}
+          userPermissions={permissions}
+        />
       </div>
       <div className="flex items-center gap-x-3">
         {!isPro && (
