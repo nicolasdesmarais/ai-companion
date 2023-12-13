d04f2cc15014ca81cf58cadbd11a4159b2b1fcae
Dmitry Torba <dmitrytorba@gmail.com>
2023-12-13 09:56:56 -0800

    feat(AIA-65) resize panel

---
+++ components/resize-panel.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { cn } from "@/src/lib/utils";
+import { ReactNode, useCallback, useRef, useState, useEffect } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+export const ResizePanel = ({ children, className }: Props) => {
+  const sidebarRef = useRef(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(360);
+
+  const startResizing = useCallback((e: any) => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback((e: any) => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    (mouseMoveEvent: any) => {
+      if (isResizing) {
+        setSidebarWidth((width) => width + mouseMoveEvent.movementX * 1.9);
+      }
+    },
+    [isResizing]
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
+
+  return (
+    <div
+      className={cn(className, "flex max-w-2xl min-w-[78px]")}
+      ref={sidebarRef}
+      style={{ width: sidebarWidth }}
+      onMouseDown={(e) => e.preventDefault()}
+    >
+      {children}
+      <div
+        className={cn(
+          "hover:bg-ring w-1 cursor-col-resize",
+          isResizing && "bg-ring"
+        )}
+        onMouseDown={startResizing}
+      ></div>
+    </div>
+  );
+};
