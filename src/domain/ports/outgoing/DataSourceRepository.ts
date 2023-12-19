42f15fc1c89afbfa6eeb976c43cc34d44f16358e
Fivos Constantinou <fivos.constantinou@appdirect.com>
2023-12-19 16:54:07 +0200

    Move initialize data source to DataSourceRepository

--- src/domain/ports/outgoing/DataSourceRepository.ts
+++ src/domain/ports/outgoing/DataSourceRepository.ts
@@ -1,3 +1,4 @@
+import { DataSourceType } from "@prisma/client";
 import { DataSourceDto } from "../../models/DataSources";
 
 export interface DataSourceRepository {
@@ -5,4 +6,12 @@ export interface DataSourceRepository {
   findByOrgId(orgId: string): Promise<DataSourceDto[]>;
   findByOrgIdAndUserId(orgId: string, userId: string): Promise<DataSourceDto[]>;
   findByAiId(aiId: string): Promise<DataSourceDto[]>;
+
+  initializeDataSource(
+    orgId: string,
+    ownerUserId: string,
+    name: string,
+    type: DataSourceType,
+    data: any
+  ): Promise<DataSourceDto>;
 }
