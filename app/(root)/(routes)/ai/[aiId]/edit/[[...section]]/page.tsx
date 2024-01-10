83b80fcf0280540c132de057e35fa48016659148
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-09 17:59:48 -0800

    chore(perf) destructive test

--- app/(root)/(routes)/ai/[aiId]/edit/[[...section]]/page.tsx
+++ app/(root)/(routes)/ai/[aiId]/edit/[[...section]]/page.tsx
@@ -1,16 +1,4 @@
 import { AIEditor } from "@/components/ai-editor";
-import { GroupModal } from "@/components/group-modal";
-import aiModelService from "@/src/domain/services/AIModelService";
-import aiService from "@/src/domain/services/AIService";
-import groupService from "@/src/domain/services/GroupService";
-import prismadb from "@/src/lib/prismadb";
-import { SecuredAction } from "@/src/security/models/SecuredAction";
-import { SecuredResourceAccessLevel } from "@/src/security/models/SecuredResourceAccessLevel";
-import { SecuredResourceType } from "@/src/security/models/SecuredResourceType";
-import { BaseEntitySecurityService } from "@/src/security/services/BaseEntitySecurityService";
-import { getUserAuthorizationContext } from "@/src/security/utils/securityUtils";
-import { redirectToSignIn } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 
 export const maxDuration = 300;
 
@@ -21,46 +9,15 @@ interface AIIdPageProps {
 }
 
 const AIIdPage = async ({ params }: AIIdPageProps) => {
-  const authorizationContext = getUserAuthorizationContext();
-  if (!authorizationContext) {
-    return redirectToSignIn();
-  }
-
-  let initialAi = null;
-  if (params.aiId !== "new") {
-    initialAi = await aiService.findAIForUser(
-      authorizationContext,
-      params.aiId
-    );
-
-    if (initialAi === null) {
-      return redirect("/");
-    }
-  }
-
-  const models = await aiModelService.getAIModels();
-
-  const categories = await prismadb.category.findMany();
-
-  const groups = await groupService.findGroupsByUser(authorizationContext);
-
-  const hasInstanceAccess = BaseEntitySecurityService.hasPermission(
-    authorizationContext,
-    SecuredResourceType.AI,
-    SecuredAction.WRITE,
-    SecuredResourceAccessLevel.INSTANCE
-  );
-
   return (
     <>
       <AIEditor
-        initialAi={initialAi}
-        aiModels={models}
-        categories={categories}
-        groups={groups}
-        hasInstanceAccess={hasInstanceAccess}
+        initialAi={null}
+        aiModels={[]}
+        categories={[]}
+        groups={[]}
+        hasInstanceAccess={true}
       />
-      <GroupModal />
     </>
   );
 };
