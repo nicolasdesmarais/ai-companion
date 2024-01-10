1f0df9b42a3ddbc868939a25b742cd2b849709dd
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-09 17:37:04 -0800

    Revert "chore(perf) time edit ai page"
    This reverts commit 839dec5299072b35f7d5bc057c3dfc760f40545d.

--- app/(root)/(routes)/ai/[aiId]/edit/[[...section]]/page.tsx
+++ app/(root)/(routes)/ai/[aiId]/edit/[[...section]]/page.tsx
@@ -19,55 +19,37 @@ interface AIIdPageProps {
     aiId: string;
   };
 }
-let start, end;
+
 const AIIdPage = async ({ params }: AIIdPageProps) => {
-  start = performance.now();
   const authorizationContext = getUserAuthorizationContext();
-  end = performance.now();
-  console.log(`getUserAuthorizationContext time: ${end - start} ms`);
   if (!authorizationContext) {
     return redirectToSignIn();
   }
 
   let initialAi = null;
   if (params.aiId !== "new") {
-    start = performance.now();
     initialAi = await aiService.findAIForUser(
       authorizationContext,
       params.aiId
     );
-    end = performance.now();
-    console.log(`findAIForUser time: ${end - start} ms`);
 
     if (initialAi === null) {
       return redirect("/");
     }
   }
 
-  start = performance.now();
   const models = await aiModelService.getAIModels();
-  end = performance.now();
-  console.log(`getAIModels time: ${end - start} ms`);
 
-  start = performance.now();
   const categories = await prismadb.category.findMany();
-  end = performance.now();
-  console.log(`category time: ${end - start} ms`);
 
-  start = performance.now();
   const groups = await groupService.findGroupsByUser(authorizationContext);
-  end = performance.now();
-  console.log(`findGroupsByUser time: ${end - start} ms`);
 
-  start = performance.now();
   const hasInstanceAccess = BaseEntitySecurityService.hasPermission(
     authorizationContext,
     SecuredResourceType.AI,
     SecuredAction.WRITE,
     SecuredResourceAccessLevel.INSTANCE
   );
-  end = performance.now();
-  console.log(`hasPermission time: ${end - start} ms`);
 
   return (
     <>
