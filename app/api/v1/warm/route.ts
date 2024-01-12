887025f7542163f28cdcfbe0e7952b08f4660ee5
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-11 18:26:48 -0800

    feat(perf) clean up warmer

--- app/api/v1/warm/route.ts
+++ app/api/v1/warm/route.ts
@@ -1,26 +1,26 @@
+import { withErrorHandler } from "@/src/middleware/ErrorMiddleware";
 import { NextRequest, NextResponse } from "next/server";
+
+export const maxDuration = 300;
 
 const paths = ["/", "/ai/new/edit", "/chat/"];
 
 const fetchPath = async (origin: string, path: string) => {
-  const resp = await fetch(origin + path, {
+  const url = origin + path;
+  const resp = await fetch(url, {
     headers: { Authorization: `Bearer ${process.env.AUTOMATION_TOKEN}` },
   });
   return {
     status: resp.status,
-    text: await resp.text(),
-    path,
+    url,
   };
 };
 
-export async function GET(req: NextRequest) {
-  try {
-    const { origin } = new URL(req.url);
-    const promises = paths.map((path) => fetchPath(origin, path));
-    const results = await Promise.all(promises);
-    return new NextResponse(JSON.stringify(results), { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
+async function getHandler(req: NextRequest) {
+  const { origin } = new URL(req.url);
+  const promises = paths.map((path) => fetchPath(origin, path));
+  const results = await Promise.all(promises);
+  return new NextResponse(JSON.stringify(results), { status: 200 });
 }
+
+export const GET = withErrorHandler(getHandler);
