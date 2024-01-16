8f60386a70f9edd8137e1ea69f9c7453c7243cef
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-16 11:57:35 -0800

    feat(AIA-79) save ms bearer

--- src/adapter-out/oauth/MsftOAuthAdapter.ts
+++ src/adapter-out/oauth/MsftOAuthAdapter.ts
@@ -1,16 +1,32 @@
 import { OAuthAdapter, TokensFromRedirect } from "./OAuthAdapter";
 import { OAuthTokenInfo } from "./OAuthTokenInfo";
 
+const scope = "offline_access%20user.read";
+const msftUrl = "https://login.microsoftonline.com/common/oauth2/v2.0";
 export class MsftOAuthAdapter implements OAuthAdapter {
   public getOAuthRedirectUrl(): string {
-    return `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${process.env.MSFT_CLIENT_ID}&redirect_uri=${process.env.MSFT_REDIRECT}&response_type=code&response_mode=query&scope=offline_access%20user.read`;
+    return `${msftUrl}/authorize?client_id=${process.env.MSFT_CLIENT_ID}&redirect_uri=${process.env.MSFT_REDIRECT}&response_type=code&response_mode=query&scope=${scope}`;
   }
 
   public async getTokensFromRedirect(
     clientCredentialData: any,
     searchParams: URLSearchParams
   ): Promise<TokensFromRedirect> {
-    return { email: "", tokens: null };
+    const code = searchParams.get("code");
+    const resp = await fetch(msftUrl + "/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `client_id=${clientCredentialData.clientId}&scope=${scope}&code=${code}&redirect_uri=${clientCredentialData.redirectUri}&grant_type=authorization_code&client_secret=${clientCredentialData.clientSecret}`,
+    });
+    const { access_token, refresh_token, error, error_description } =
+      await resp.json();
+
+    if (error) {
+      throw new Error(`MSFT Error: ${error} - ${error_description}`);
+    }
+    return { email: "", tokens: { access_token, refresh_token } };
   }
 
   public async getOAuthTokenInfo(
