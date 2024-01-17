12dbed2bcd9e69ef1170ab7bc01b0703b756f432
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-17 15:16:09 -0800

    feat(AIA-79) index onedrive

--- components/svg/onedrive-svg.tsx
+++ components/svg/onedrive-svg.tsx
@@ -16,7 +16,7 @@ export const OneDriveSvg = ({ fill, className }: Props) => {
       stroke="none"
       fill={fill || "currentColor"}
     >
-      <g stroke-width="1" fill-rule="evenodd">
+      <g strokeWidth="1" fillRule="evenodd">
         <g transform="translate(-220.000000, -7519.000000)">
           <g transform="translate(56.000000, 160.000000)">
             <path d="M174,7379 L184,7379 L184,7370 L174,7370 L174,7379 Z M164,7379 L173,7379 L173,7370 L164,7370 L164,7379 Z M174,7369 L184,7369 L184,7359 L174,7359 L174,7369 Z M164,7369 L173,7369 L173,7359 L164,7359 L164,7369 Z"></path>
--- src/adapter-out/knowledge/msft/MsftDataSourceAdapter.ts
+++ src/adapter-out/knowledge/msft/MsftDataSourceAdapter.ts
@@ -10,7 +10,10 @@ import {
   DataSourceItemList,
 } from "../types/DataSourceItemList";
 import { IndexKnowledgeResponse } from "../types/IndexKnowledgeResponse";
-import { KnowledgeIndexingResult } from "../types/KnowlegeIndexingResult";
+import {
+  KnowledgeIndexingResult,
+  KnowledgeIndexingResultStatus,
+} from "../types/KnowlegeIndexingResult";
 import axios from "axios";
 import prismadb from "@/src/lib/prismadb";
 import {
@@ -18,6 +21,10 @@ import {
   ForbiddenError,
 } from "@/src/domain/errors/Errors";
 import { decryptFromBuffer } from "@/src/lib/encryptionUtils";
+import { Readable } from "stream";
+import { put } from "@vercel/blob";
+import { publishEvent } from "@/src/adapter-in/inngest/event-publisher";
+import { DomainEvent } from "@/src/domain/events/domain-event";
 
 export class MsftDataSourceAdapter implements DataSourceAdapter {
   private static readonly GraphApiUrl = "https://graph.microsoft.com/v1.0";
@@ -99,13 +106,12 @@ export class MsftDataSourceAdapter implements DataSourceAdapter {
           modifiedTime: item.lastModifiedDateTime,
         },
       };
-      console.log(item, dataSourceItem);
+      return {
+        type: DataSourceType.ONEDRIVE,
+        items: [dataSourceItem],
+      };
     }
     throw new Error("Method not implemented.");
-    return {
-      type: DataSourceType.ONEDRIVE,
-      items: [],
-    };
   }
 
   public async indexKnowledge(
@@ -114,14 +120,96 @@ export class MsftDataSourceAdapter implements DataSourceAdapter {
     knowledge: Knowledge,
     data: any
   ): Promise<IndexKnowledgeResponse> {
-    throw new Error("Method not implemented.");
+    if (knowledge.indexStatus === KnowledgeIndexStatus.COMPLETED) {
+      return {
+        indexStatus: KnowledgeIndexStatus.COMPLETED,
+      };
+    }
+    if (!userId) {
+      console.error("Missing userId");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+    if (!data?.oauthTokenId) {
+      console.error("Missing oauthTokenId");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+
+    const token = await this.getToken(userId, data.oauthTokenId);
+    const item = await this.fetch(token, `/me/drive/items/${data.fileId}`);
+
+    const downloadUrl = item["@microsoft.graph.downloadUrl"];
+    if (!downloadUrl) {
+      console.error("Missing downloadUrl");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+    const response = await fetch(downloadUrl);
+    if (!response.body || response.status !== 200) {
+      console.error("msft indexKnowledge: download fail");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+    const blob = await response.blob();
+
+    const { docs, metadata } = await fileLoader.getLangchainDocs(
+      knowledge.id,
+      item.name,
+      "",
+      blob
+    );
+
+    const cloudBlob = await put(
+      `${knowledge.name}.json`,
+      JSON.stringify(docs),
+      {
+        access: "public",
+      }
+    );
+    knowledge.blobUrl = cloudBlob.url;
+
+    let eventIds: string[] = [];
+    for (let i = 0; i < docs.length; i++) {
+      const eventResult = await publishEvent(
+        DomainEvent.KNOWLEDGE_CHUNK_RECEIVED,
+        {
+          knowledgeIndexingResult: {
+            knowledgeId: knowledge.id,
+            result: {
+              chunkCount: docs.length,
+              status: KnowledgeIndexingResultStatus.SUCCESSFUL,
+            },
+          },
+          dataSourceType: DataSourceType.ONEDRIVE,
+          index: i,
+        }
+      );
+      eventIds = eventIds.concat(eventResult.ids);
+    }
+    return {
+      userId,
+      indexStatus: KnowledgeIndexStatus.INDEXING,
+      documentCount: metadata.documentCount,
+      tokenCount: metadata.totalTokenCount,
+      metadata: {
+        eventIds,
+        ...metadata,
+      },
+    };
   }
 
   public shouldReindexKnowledge(
     knowledge: Knowledge,
     item: DataSourceItem
   ): boolean {
-    throw new Error("Method not implemented.");
+    return (
+      (knowledge.metadata as any)?.modifiedTime !== item.metadata.modifiedTime
+    );
   }
 
   public retrieveKnowledgeIdFromEvent(data: any): string {
@@ -140,7 +228,49 @@ export class MsftDataSourceAdapter implements DataSourceAdapter {
     result: KnowledgeIndexingResult,
     index: number
   ): Promise<IndexKnowledgeResponse> {
-    throw new Error("Method not implemented.");
+    if (!knowledge.blobUrl) {
+      console.error("msft loadKnowledgeResult: blob fail");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+
+    const response = await fetch(knowledge.blobUrl);
+    if (response.status !== 200) {
+      console.error("msft loadKnowledgeResult: fetch fail");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+    const data = await response.json();
+    if (!data || !data[index]) {
+      console.error("msft loadKnowledgeResult: data fail");
+      return {
+        indexStatus: KnowledgeIndexStatus.FAILED,
+      };
+    }
+
+    const docs = [data[index]];
+    const metadata = await fileLoader.loadDocs(docs);
+
+    console.log("msft chunk loading", knowledge.id, index);
+
+    let indexStatus;
+    switch (result.status) {
+      case KnowledgeIndexingResultStatus.SUCCESSFUL:
+      case KnowledgeIndexingResultStatus.PARTIAL:
+        indexStatus = KnowledgeIndexStatus.PARTIALLY_COMPLETED;
+        break;
+      case KnowledgeIndexingResultStatus.FAILED:
+        indexStatus = KnowledgeIndexStatus.FAILED;
+    }
+    return {
+      indexStatus,
+      metadata: {
+        ...metadata,
+        completedChunks: [index],
+      },
+    };
   }
 
   public async pollKnowledgeIndexingStatus(
