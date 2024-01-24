59483b6b1a8fba8751fd7861969917414dace8a3
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-01-24 18:37:13 +0200

    refactor Base chat refactor (#218)
    * Rename BaseChatModel
    
    * Use langchain BaseChatModel from model instance

--- src/adapter-out/ai-model/chat-models/Gpt35Model.ts
+++ src/adapter-out/ai-model/chat-models/Gpt35Model.ts
@@ -1,8 +1,6 @@
-import {
-  ChatOpenAI,
-  ChatOpenAICallOptions,
-} from "langchain/chat_models/openai";
-import { BaseChatModel } from "./BaseChatModel";
+import { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { AbstractBaseChatModel } from "./AbstractBaseChatModel";
 import { ChatModel } from "./ChatModel";
 
 const MODEL_ID = "gpt35-16k";
@@ -12,14 +10,12 @@ const AZURE_OPENAI_API_VERSION = "2023-05-15";
 const AZURE_OPENAI_API_INSTANCE_NAME = "appdirect-prod-ai-useast";
 const AZURE_OPENAI_API_DEPLOYMENT_NAME = "ai-prod-16k";
 
-export class Gpt35Model extends BaseChatModel implements ChatModel {
+export class Gpt35Model extends AbstractBaseChatModel implements ChatModel {
   public supports(modelId: string): boolean {
     return modelId === MODEL_ID;
   }
 
-  protected getChatModelInstance(
-    options: any
-  ): ChatOpenAI<ChatOpenAICallOptions> {
+  protected getChatModelInstance(options: any): BaseChatModel {
     return new ChatOpenAI({
       azureOpenAIApiKey: process.env.AZURE_GPT35_KEY,
       azureOpenAIApiVersion: AZURE_OPENAI_API_VERSION,
--- src/adapter-out/ai-model/chat-models/Gpt4Model.ts
+++ src/adapter-out/ai-model/chat-models/Gpt4Model.ts
@@ -1,8 +1,6 @@
-import {
-  ChatOpenAI,
-  ChatOpenAICallOptions,
-} from "langchain/chat_models/openai";
-import { BaseChatModel } from "./BaseChatModel";
+import { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { AbstractBaseChatModel } from "./AbstractBaseChatModel";
 import { ChatModel } from "./ChatModel";
 
 const MODEL_ID = "gpt-4";
@@ -12,14 +10,12 @@ const AZURE_OPENAI_API_VERSION = "2023-05-15";
 const AZURE_OPENAI_API_INSTANCE_NAME = "prod-appdirectai-east2";
 const AZURE_OPENAI_API_DEPLOYMENT_NAME = "gpt4-32k";
 
-export class Gpt4Model extends BaseChatModel implements ChatModel {
+export class Gpt4Model extends AbstractBaseChatModel implements ChatModel {
   public supports(modelId: string): boolean {
     return modelId === MODEL_ID;
   }
 
-  protected getChatModelInstance(
-    options: any
-  ): ChatOpenAI<ChatOpenAICallOptions> {
+  protected getChatModelInstance(options: any): BaseChatModel {
     return new ChatOpenAI({
       azureOpenAIApiKey: process.env.AZURE_GPT40_KEY,
       azureOpenAIApiVersion: AZURE_OPENAI_API_VERSION,
