dadd9047e26561bbb1be2838c647f2a01e93e684
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-29 14:48:37 -0800

    feat(AIA-69) start data stores page

---
+++ app/(root)/(routes)/data-sources/page.tsx
@@ -0,0 +1,12 @@
+import { DataStores } from "@/components/data-stores";
+
+export default async function DataSourcesPage() {
+  return (
+    <div className="pl-2 pr-4">
+      <h1 className="text-3xl font-bold whitespace-nowrap pt-2 pr-2">
+        Data Stores
+      </h1>
+      <DataStores />
+    </div>
+  );
+}
---
+++ components/data-stores.tsx
@@ -0,0 +1,9 @@
+export const DataStores = () => {
+  return (
+    <div>
+      <div>Search</div>
+      <div>Table</div>
+      <div>Details</div>
+    </div>
+  );
+};
