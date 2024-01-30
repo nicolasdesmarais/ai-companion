8eee28f340a409dfd43aa4786cda691c9f420fe1
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-29 16:48:22 -0800

    feat(AIA-69) start data stores components

---
+++ components/data-stores-table.tsx
@@ -0,0 +1,196 @@
+"use client";
+import { useEffect } from "react";
+import { Table } from "@/components/table";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import axios, { AxiosError } from "axios";
+import { format } from "date-fns";
+import { ChevronRight, ChevronDown, Loader, MinusCircle } from "lucide-react";
+import { useState } from "react";
+import { DataSourceTypes } from "./datasource-types";
+import { KnowledgeIndexStatus } from "@prisma/client";
+import Link from "next/link";
+
+export const DataStoresTable = () => {
+  const [dataSources, setDataSources] = useState<any[]>([]);
+  const [removing, setRemoving] = useState("");
+  const [expanded, setExpanded] = useState<string[]>([]);
+  const { toast } = useToast();
+
+  const removeDataSource = async (id: string) => {};
+
+  const fetchDataSources = async () => {
+    try {
+      const response = await axios.get(`/api/v1/super/data-sources/`);
+      setDataSources(response.data);
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        description:
+          String((error as AxiosError).response?.data) ||
+          "Something went wrong.",
+        duration: 6000,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchDataSources();
+  }, []);
+
+  return (
+    <div className="mt-2">
+      <Table
+        headers={[
+          "NAME",
+          "AIs",
+          "TYPE",
+          "LAST MODIFIED",
+          "Progress",
+          "Status",
+          "Remove",
+        ]}
+        className="w-full my-4 max-h-60"
+      >
+        {dataSources.map((dataSource: any) => (
+          <>
+            <tr
+              key={dataSource.id}
+              className="items-center my-2 text-sm hover:bg-ring/10"
+            >
+              <td className="p-2 ">
+                <div
+                  onClick={() => toggle(dataSource.id)}
+                  className=" truncate text-ring cursor-pointer flex items-center"
+                >
+                  {expanded.includes(dataSource.id) ? (
+                    <ChevronDown className="w-4 h-4 mr-2 text-white" />
+                  ) : (
+                    <ChevronRight className="w-4 h-4 mr-2 text-white" />
+                  )}
+                  {dataSource.name}
+                </div>
+              </td>
+              <td className="p-2 max-w-sm truncate">
+                {dataSource.ais.map((ai: any) => {
+                  return (
+                    <Link
+                      key={ai.ai.id}
+                      target="_blank"
+                      href={`/ai/${ai.ai.id}`}
+                      className="text-ring"
+                    >
+                      {ai.ai.name}
+                    </Link>
+                  );
+                })}
+              </td>
+              <td className="p-2">
+                {
+                  DataSourceTypes.find(
+                    (format) => format.type === dataSource.type
+                  )?.name
+                }
+              </td>
+              <td className="p-2">
+                {dataSource.lastIndexedAt
+                  ? format(
+                      new Date(dataSource.lastIndexedAt),
+                      "h:mma M/d/yyyy "
+                    )
+                  : null}
+              </td>
+              <td className="p-2">
+                {dataSource.indexStatus === KnowledgeIndexStatus.FAILED
+                  ? "Failed"
+                  : Math.round(dataSource.indexPercentage) + "%"}
+              </td>
+              <td className="p-2">{dataSource.indexStatus}</td>
+              <td className="p-2 text-center">
+                <Button
+                  type="button"
+                  variant="outline"
+                  disabled={!!removing}
+                  onClick={() => removeDataSource(dataSource.id)}
+                >
+                  {removing === dataSource.id ? (
+                    <Loader className="w-4 h-4 spinner" />
+                  ) : (
+                    <MinusCircle className="w-4 h-4 text-destructive" />
+                  )}
+                </Button>
+              </td>
+            </tr>
+            {expanded.includes(dataSource.id) &&
+              dataSource.knowledges.map(({ knowledge }: any) => (
+                <tr
+                  key={knowledge.id}
+                  className="items-center my-2 text-sm hover:bg-ring/10"
+                >
+                  <td className="p-2 pl-10">
+                    <div className="max-w-sm truncate">
+                      {knowledge.metadata?.indexingRunId ? (
+                        <Link
+                          target="_blank"
+                          href={`https://console.apify.com/organization/Xn4BErd8aMtmstvY2/actors/moJRLRc85AitArpNN/runs/${knowledge.metadata.indexingRunId}`}
+                          className="text-ring"
+                        >
+                          {knowledge.name}
+                        </Link>
+                      ) : (
+                        knowledge.name
+                      )}
+                    </div>
+                  </td>
+                  <td className="p-2">
+                    {dataSource.ais.map((ai: any) => {
+                      return (
+                        <Link
+                          key={ai.ai.id}
+                          target="_blank"
+                          href={`/ai/${ai.ai.id}`}
+                          className="text-ring"
+                        >
+                          {ai.ai.name}
+                        </Link>
+                      );
+                    })}
+                  </td>
+                  <td className="p-2">{knowledge.type}</td>
+                  <td className="p-2">
+                    {knowledge.lastIndexedAt
+                      ? format(
+                          new Date(knowledge.lastIndexedAt),
+                          "h:mma M/d/yyyy "
+                        )
+                      : null}
+                  </td>
+                  <td
+                    onClick={() => onCopy(dataSource.id)}
+                    className="text-ring cursor-pointer"
+                  >
+                    {knowledge.id}
+                  </td>
+
+                  <td className="p-2 text-center">
+                    <Button
+                      type="button"
+                      variant="outline"
+                      disabled={!!removing}
+                      onClick={() => removeDataSource(knowledge.id)}
+                    >
+                      {removing === knowledge.id ? (
+                        <Loader className="w-4 h-4 spinner" />
+                      ) : (
+                        <MinusCircle className="w-4 h-4 text-destructive" />
+                      )}
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+          </>
+        ))}
+      </Table>
+    </div>
+  );
+};
