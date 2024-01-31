a5afe5ec877e8f41bb14e5c1e1af1ea2f3673c9a
Dmitry Torba <dmitrytorba@gmail.com>
2024-01-31 08:07:17 -0800

    feat(datadog) move to azure

--- components/datadog-init.tsx
+++ components/datadog-init.tsx
@@ -3,9 +3,9 @@
 import { datadogRum } from "@datadog/browser-rum";
 
 datadogRum.init({
-  applicationId: "c4cdbfab-7db8-452b-a7ba-37c251189ee2",
-  clientToken: "pub3fb1d50e7a6f5444ff86c55271220170",
-  site: "datadoghq.com",
+  applicationId: "03817d82-3867-4802-b0d7-b9e8bd374b98",
+  clientToken: "pub17023fc825ff17c93288e95d1442a6c1",
+  site: "us3.datadoghq.com",
   service: "appdirect.ai",
   env: process.env.NEXT_PUBLIC_VERCEL_ENV || "local",
   version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "local",
