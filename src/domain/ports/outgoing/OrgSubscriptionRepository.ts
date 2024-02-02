8b1b04af6beeef8d80f8ec048153ee060b0e19d4
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-02-02 13:15:31 +0200

    feat Implement API to retrieve org usage (#232)
    * Implement API to retrieve org usage
    
    * OpenAPI spec for usage API
    
    * Add missing await
    
    * Format token count result as number

--- src/domain/ports/outgoing/OrgSubscriptionRepository.ts
+++ src/domain/ports/outgoing/OrgSubscriptionRepository.ts
@@ -1,5 +1,6 @@
 import { OrgSubscriptionDto } from "../../models/OrgSubscriptions";
 
 export interface OrgSubscriptionRepository {
+  findByOrgId(orgId: string): Promise<OrgSubscriptionDto | null>;
   findOrCreateByOrgId(orgId: string): Promise<OrgSubscriptionDto | null>;
 }
