f022749145d92b7a95c6f64b2378f433217c75ba
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-02-06 12:59:19 +0200

    Add cohere model (#237)

---
+++ src/adapter-out/ai-model/chat-models/CohereModel.ts
@@ -0,0 +1,21 @@
+import { AIModel } from "@/src/domain/models/AIModel";
+import { ChatCohere } from "@langchain/cohere";
+import { BaseChatModel as BaseLangChainChatModel } from "@langchain/core/language_models/chat_models";
+import { AbstractBaseChatModel } from "./AbstractBaseChatModel";
+import { ChatModel } from "./ChatModel";
+
+const MODEL_ID = "cohere";
+
+export class CohereModel extends AbstractBaseChatModel implements ChatModel {
+  public supports(model: AIModel): boolean {
+    return model.id === MODEL_ID;
+  }
+
+  protected getChatModelInstance(options: any): BaseLangChainChatModel {
+    return new ChatCohere({
+      apiKey: process.env.COHERE_API_KEY,
+      model: "command",
+      ...options,
+    });
+  }
+}
