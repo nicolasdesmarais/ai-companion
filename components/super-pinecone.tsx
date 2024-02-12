c483b73ec5aceb94bacbe83059af2bdfd0c140af
Dmitry Torba <dmitrytorba@gmail.com>
2024-02-12 08:23:49 -0800

    feat(super) pinecone api update

--- components/super-pinecone.tsx
+++ components/super-pinecone.tsx
@@ -1,15 +1,8 @@
 "use client";
-import { useEffect } from "react";
-import { Table } from "@/components/table";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import axios, { AxiosError } from "axios";
-import { format } from "date-fns";
-import { ChevronRight, ChevronDown, Loader, MinusCircle } from "lucide-react";
-import { useState } from "react";
-import { DataSourceTypes } from "./datasource-types";
-import { KnowledgeIndexStatus } from "@prisma/client";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import { Banner } from "./ui/banner";
 
 export const SuperPinecone = () => {
@@ -76,25 +69,20 @@ export const SuperPinecone = () => {
       <h2 className="text-lg font-medium">Pinecone Indexes</h2>
       <div className="text-muted-foreground text-sm">
         {indexes.map(({ indexDescription, indexStats, isCurrent }: any) => (
-          <div key={indexDescription.database.name} className="pt-4">
-            <h3 className="text-lg font-medium">
-              {indexDescription.database.name}
-            </h3>
+          <div key={indexDescription.name} className="pt-4">
+            <h3 className="text-lg font-medium">{indexDescription.name}</h3>
             {isCurrent ? (
               <Banner className="w-80 my-2">
                 This environment is using this index.
               </Banner>
             ) : null}
-            <div>Pods: {indexDescription.database.pods}</div>
-            <div>Replicas: {indexDescription.database.replicas}</div>
-            <div>Shards: {indexDescription.database.shards}</div>
-            <div>Pod Type: {indexDescription.database.podType}</div>
+            <div>Pods: {indexDescription.spec.pod.pods}</div>
+            <div>Replicas: {indexDescription.spec.pod.replicas}</div>
+            <div>Shards: {indexDescription.spec.pod.shards}</div>
+            <div>Pod Type: {indexDescription.spec.pod.podType}</div>
             <div>Fullness: {indexStats.indexFullness}</div>
             <div>Total Record Count: {indexStats.totalRecordCount}</div>
-            <Button
-              className="mt-4"
-              onClick={backup(indexDescription.database.name)}
-            >
+            <Button className="mt-4" onClick={backup(indexDescription.name)}>
               Backup
             </Button>
           </div>
@@ -103,12 +91,12 @@ export const SuperPinecone = () => {
 
       <h3 className="text-lg font-medium">Pinecone Backups</h3>
       <div className="text-muted-foreground text-sm">
-        {collections.map(({ name, size, status, recordCount }: any) => (
+        {collections.map(({ name, size, status, vectorCount }: any) => (
           <div key={name} className="pt-4">
             <h3 className="text-lg font-medium">{name}</h3>
             <div>Size: {Math.round(size / 1000000)} Mb</div>
             <div>Status: {status}</div>
-            <div>Vector Count: {recordCount}</div>
+            <div>Vector Count: {vectorCount}</div>
             <Button
               className="mt-4"
               variant="destructive"
