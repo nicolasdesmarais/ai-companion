6a45b65bcbcae6aa0fee5e9b4ea076ecb24d23ec
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-01 07:35:54 -0800

    feat(AIA-111) mobile topnav for public

--- components/public-ai-chat.tsx
+++ components/public-ai-chat.tsx
@@ -1,4 +1,6 @@
 import aiService from "@/src/domain/services/AIService";
+import { Menu } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { PublicChatClient } from "./public-chat-client";
 import { PublicSidebar } from "./public-sidebar";
@@ -17,10 +19,15 @@ export const PublicAiChat = async ({ aiId }: Props) => {
 
     return (
       <div className="h-full">
+        <div className="absolute pt-7 pl-5 md:hidden pr-4 z-50">
+          <Link href="/sign-up" className="flex">
+            <Menu />
+          </Link>
+        </div>
         <div className="hidden md:flex h-full w-20 flex-col fixed inset-y-0 z-40">
           <PublicSidebar isChat={true} ais={ais} />
         </div>
-        <main className="md:pl-20 pt-20 md:pt-0 h-full">
+        <main className="md:pl-20 md:pt-0 h-full">
           <PublicChatClient ai={ai} ais={ais} />
         </main>
       </div>
