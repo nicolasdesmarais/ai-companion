573ce7d1e909a110b1749dcfd38b65d5639eb3ea
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-04 10:45:07 -0800

    feat(public) metadata tags

--- app/public/ai/[aiId]/page.tsx
+++ app/public/ai/[aiId]/page.tsx
@@ -1,4 +1,6 @@
 import { PublicAiChat } from "@/components/public-ai-chat";
+import aiService from "@/src/domain/services/AIService";
+import type { Metadata, ResolvingMetadata } from "next";
 
 interface Props {
   params: {
@@ -8,5 +10,36 @@ interface Props {
 const PublicAiPage = async ({ params }: Props) => {
   return <PublicAiChat aiId={params.aiId} />;
 };
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const ais = await aiService.findPublicAIs();
+  const ai = ais.find((ai) => ai.id === params.aiId);
+
+  if (ai) {
+    return {
+      title: `${ai.name} | AppDirect AI`,
+      openGraph: {
+        images: [
+          {
+            url: ai.src,
+            width: 512,
+            height: 512,
+          },
+        ],
+        description: ai.description,
+        url: `https://appdirect.ai/public/ai/${ai.id}`,
+        siteName: "AppDirect AI",
+        locale: "en_US",
+        type: "website",
+      },
+    };
+  } else
+    return {
+      title: "AppDirect AI",
+    };
+}
 
 export default PublicAiPage;
