cf0d8bb37348bbcbf5159518a5f028d31b83397d
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-08 14:27:24 -0800

    feat(landing) metadata

--- app/landing/layout.tsx
+++ app/landing/layout.tsx
@@ -1,5 +1,23 @@
 import { PublicAnalytics } from "@/components/analytics";
 
+export const metadata = {
+  title: `AppDirect AI`,
+  openGraph: {
+    images: [
+      {
+        url: "/browse_screenshot.jpg",
+        width: "512",
+        height: "377",
+      },
+    ],
+    description:
+      "Transform your AI app ideas into reality without needing any coding skills. Unlock innovation and productivity for you, your team, and your customers.",
+    siteName: "AppDirect AI Marketplace & Creation Studio",
+    locale: "en_US",
+    type: "website",
+  },
+};
+
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="h-full bg-[#111827] overflow-auto">
