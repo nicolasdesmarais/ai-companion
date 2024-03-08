af2fd74c8e1a74361ea0b234afee960a5a532e8b
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-08 09:29:30 -0800

    feat(landing) footer mobile

--- components/landing-footer.tsx
+++ components/landing-footer.tsx
@@ -6,73 +6,75 @@ const LandingFooter = () => {
   return (
     <div className="flex text-white bg-navy justify-center w-full mt-20 py-8">
       <nav className="flex my-4 w-[1144px] justify-between">
-        <div className="w-full">
-          <div className="flex gap-20">
-            <div className="flex mr-40">
+        <div className="w-full mx-8">
+          <div className="flex-col lg:flex-row flex">
+            <div className="flex mr-40 mb-8">
               <AppdirectSvg className="h-5 w-5" fill="white" />
               AppDirect <span className="font-extrabold ml-2">AI</span>
             </div>
-            <div>
-              <div className="font-bold">PRODUCT</div>
-              <ul className="flex flex-col justify-between gap-4 py-2 font-light">
-                <li>
-                  <Link href="/landing/features">Features</Link>
-                </li>
-                <li>
-                  <Link href="/landing/pricing">Pricing</Link>
-                </li>
-                <li>
-                  <Link href="/landing/enterprise">Enterprise</Link>
-                </li>
-                <li>
-                  <Link href="/landing/resources">Resources</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <div className="font-bold">RESOURCES</div>
-              <ul className="flex flex-col justify-between gap-4 py-2 font-light">
-                <li>
-                  <Link href="/landing/home">Tutorials</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">User Guides</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">API Guides</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">FAQ</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <div className="font-bold">LEGAL</div>
-              <ul className="flex flex-col justify-between gap-4 py-2 font-light">
-                <li>
-                  <Link href="/landing/home">Privacy Notice</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">Terms of Use</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <div className="font-bold">SOCIAL</div>
-              <ul className="flex flex-col justify-between gap-4 py-2 font-light">
-                <li>
-                  <Link href="/landing/home">LinkedIn</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">YouTube</Link>
-                </li>
-                <li>
-                  <Link href="/landing/home">Instagram</Link>
-                </li>
-              </ul>
+            <div className="flex flex-wrap gap-6 lg:gap-8">
+              <div className="w-36">
+                <div className="font-bold">PRODUCT</div>
+                <ul className="flex flex-col justify-between gap-4 py-2 font-light">
+                  <li>
+                    <Link href="/landing/features">Features</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/pricing">Pricing</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/enterprise">Enterprise</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/resources">Resources</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-36">
+                <div className="font-bold">RESOURCES</div>
+                <ul className="flex flex-col justify-between gap-4 py-2 font-light">
+                  <li>
+                    <Link href="/landing/home">Tutorials</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">User Guides</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">API Guides</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">FAQ</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-36">
+                <div className="font-bold">LEGAL</div>
+                <ul className="flex flex-col justify-between gap-4 py-2 font-light">
+                  <li>
+                    <Link href="/landing/home">Privacy Notice</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">Terms of Use</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-36">
+                <div className="font-bold">SOCIAL</div>
+                <ul className="flex flex-col justify-between gap-4 py-2 font-light">
+                  <li>
+                    <Link href="/landing/home">LinkedIn</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">YouTube</Link>
+                  </li>
+                  <li>
+                    <Link href="/landing/home">Instagram</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div className="flex justify-between w-full mt-8">
+          <div className="flex justify-between w-full mt-8 items-center">
             <div>{`AppDirect ©${currentYear}`}</div>
             <Link
               href="/sign-up"
