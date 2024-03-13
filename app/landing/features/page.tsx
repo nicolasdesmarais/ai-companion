a9ce3b786e0c1a63338037383797533d87431c89
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-13 16:01:56 -0700

    feat(landing) resources page

--- app/landing/features/page.tsx
+++ app/landing/features/page.tsx
@@ -12,7 +12,7 @@ const LandingFeatures = () => {
 
       <div className="py-10 mt-10 flex flex-col items-center bg-[#F8F8F8] ">
         <div className="lg:w-[1140px] mx-4">
-          <h2 className="text-5xl font-extrabold">How it works</h2>
+          <h2 className="text-5xl font-bold">How it works</h2>
           <div className="lg:w-[700px] mt-10 text-lg">
             Transform your AI app ideas into reality without needing any coding
             skills. Unlock innovation and productivity for you, your team, and
