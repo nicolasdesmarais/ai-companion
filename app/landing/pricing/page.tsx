05157f4211b63cc86599c273eb59a3bec8b4309d
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-18 10:29:27 -0700

    feat(landing) pricing table mobile

--- app/landing/pricing/page.tsx
+++ app/landing/pricing/page.tsx
@@ -1,29 +1,8 @@
 import LandingFAQ from "@/components/landing-faq";
 import LandingFooter from "@/components/landing-footer";
 import LandingNav from "@/components/landing-nav";
+import LandingPricing from "@/components/landing-pricing";
 import LandingSupport from "@/components/landing-support";
-import { Check } from "lucide-react";
-import Link from "next/link";
-
-const plans = [
-  {
-    name: "$10/month",
-    price: "10",
-    data: "3GB/mo",
-  },
-  {
-    name: "$100/month",
-    price: "100",
-    data: "30GB/mo",
-  },
-  {
-    name: "Custom",
-    price: "Custom",
-    data: "> 30GB/mo",
-    custom: true,
-    tuning: true,
-  },
-];
 
 const LandingFeatures = () => {
   return (
@@ -36,128 +15,7 @@ const LandingFeatures = () => {
         </div>
       </div>
 
-      <div className="flex items-center justify-center my-16">
-        <div className="lg:w-[1100px] mx-4 flex flex-col items-center">
-          <h2 className="text-3xl font-bold text-center mb-8">
-            Choose the plan that&apos;s right for you
-          </h2>
-          <Link href="/sign-up" className="px-8 py-2 bg-sky">
-            Sign up
-          </Link>
-          <div className="hidden lg:flex mt-16 justify-center">
-            <div className="grid grid-cols-2 w-1/3 bg-[#F8F8F8] pl-4">
-              <div className="border-b border-[#DDDDDD] p-8 h-20"></div>
-              <div className="font-bold border-b border-[#DDDDDD] p-8 h-20 text-center">
-                Free
-              </div>
-              <div className="border-b border-[#DDDDDD] flex items-center min-w-[120px]">
-                Data usage
-              </div>
-              <div className="font-bold h-20 text-center border-b border-[#DDDDDD] flex items-center justify-center">
-                200MB/mo
-              </div>
-              <div className="border-b border-[#DDDDDD] flex items-center">
-                Use and create unlimited AIs
-              </div>
-              <div className="font-bold h-28 text-center border-b border-[#DDDDDD] flex items-center justify-center">
-                <Check className="h-10 w-10" />
-              </div>
-              <div className="border-b border-[#DDDDDD] flex items-center">
-                Share AIs easily via email or generated link
-              </div>
-              <div className="font-bold h-28 border-b border-[#DDDDDD] flex items-center justify-center">
-                <Check className="h-10 w-10" />
-              </div>
-              <div className="border-b h-28 border-[#DDDDDD] flex items-center">
-                Multiple LLMs to choose from for each AI
-              </div>
-              <div className="font-bold h-28 border-b border-[#DDDDDD] flex items-center justify-center">
-                <Check className="h-10 w-10" />
-              </div>
-              <div className=" border-b h-28 border-[#DDDDDD] flex items-center">
-                All proprietary data is kept secure and private
-              </div>
-              <div className="font-bold h-28 border-b border-[#DDDDDD] flex items-center justify-center">
-                <Check className="h-10 w-10" />
-              </div>
-              <div className="border-b h-28 border-[#DDDDDD] flex items-center">
-                AI evaluation and custom configuration
-              </div>
-              <div className="border-b h-28 border-[#DDDDDD]"></div>
-              <div className="h-28 flex items-center">
-                Advanced integrations, i.e. LLM fine tuning and private
-                deployment
-              </div>
-            </div>
-            <div className="grid grid-cols-3 shadow-lg text-center pr-4">
-              {plans.map((plan) => (
-                <div
-                  key={`plan-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] p-8 h-20"
-                >
-                  {plan.name}
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`data-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] flex items-center justify-center h-20"
-                >
-                  {plan.data}
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`create-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] h-28 flex items-center justify-center"
-                >
-                  <Check className="h-10 w-10" />
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`share-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] h-28 flex items-center justify-center"
-                >
-                  <Check className="h-10 w-10" />
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`llms-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] h-28 flex items-center justify-center"
-                >
-                  <Check className="h-10 w-10" />
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`secure-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] h-28 flex items-center justify-center"
-                >
-                  <Check className="h-10 w-10" />
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`custom-${plan.name}`}
-                  className="font-bold border-b border-[#DDDDDD] h-28 flex items-center justify-center"
-                >
-                  {plan.custom ? <Check className="h-10 w-10" /> : null}
-                </div>
-              ))}
-              {plans.map((plan) => (
-                <div
-                  key={`tuning-${plan.name}`}
-                  className="font-bold h-28 flex items-center justify-center"
-                >
-                  {plan.tuning ? <Check className="h-10 w-10" /> : null}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingPricing />
 
       <LandingFAQ />
 
