ca5ded5c4cffdd6c5cb4486becbcff5f96f1e6f2
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-20 11:02:00 -0700

    feat(landing) enterprise hero

--- app/landing/enterprise/page.tsx
+++ app/landing/enterprise/page.tsx
@@ -1,17 +1,29 @@
 import LandingFooter from "@/components/landing-footer";
 import LandingNav from "@/components/landing-nav";
+import Image from "next/image";
 
 const LandingFeatures = () => {
   return (
     <div className="bg-white flex flex-col text-navy">
       <LandingNav />
 
-      <div className="py-10 flex flex-col items-center bg-navy text-white">
-        <div className="lg:w-[1140px] mx-4">
-          <h2 className="text-5xl font-bold">AI for the Enterprise</h2>
-          <div className="lg:w-[700px] mt-10 text-lg">
-            For companies looking to build AI-driven solutions that solve
-            complex supply chain problems.
+      <div className="py-20 flex flex-col items-center bg-navy text-white">
+        <div className="lg:w-[1140px] mx-4 flex justify-between">
+          <div className="flex flex-col justify-center">
+            <h2 className="text-5xl font-bold">AI for the Enterprise</h2>
+            <div className="lg:w-[300px] mt-10 text-lg">
+              For companies looking to build AI-driven solutions that solve
+              complex supply chain problems.
+            </div>
+          </div>
+          <div className="lg:bg-gradient1 lg:w-[572px] lg:h-[544px] flex items-center">
+            <Image
+              src="/ship.png"
+              alt="Container Ship"
+              width="451"
+              height="422"
+              className="-ml-10"
+            />
           </div>
         </div>
       </div>
