e4209193ce5b7d0bc94b84d07c1fdedf666f8bfe
Dmitry Torba <dmitrytorba@gmail.com>
2024-03-25 14:12:43 -0700

    fix(landing) firefox blob animation

--- app/(auth2)/(Routes)/login/page.tsx
+++ app/(auth2)/(Routes)/login/page.tsx
@@ -62,7 +62,7 @@ const Login = () => {
           </div>
         </div>
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="svg-filter">
           <defs>
             <filter id="goo">
               <feGaussianBlur
