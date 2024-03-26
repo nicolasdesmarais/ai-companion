7337b7c32ab7332a4b103372c0718de7ffcc4c15
vipuldunde <vipul.dunde@appdirect.com>
2024-03-26 21:46:54 +0530

    AIA-140 | Fix spaces with space-x-2.

--- components/filters.tsx
+++ components/filters.tsx
@@ -66,7 +66,7 @@ export const Filters = () => {
 
   if (scope && SuperuserScopes.includes(scope as ListAIsRequestScope)) {
     return (
-      <div className="w-full overflow-x-auto space-x-0.5 flex p-1">
+      <div className="w-full overflow-x-auto space-x-2 flex p-1">
         <div className="flex space-x-0.5">
           <button
             onClick={() => onClick("INSTANCE_ORGANIZATION")}
@@ -117,7 +117,7 @@ export const Filters = () => {
 
   if (scope && AdminScopes.includes(scope as ListAIsRequestScope)) {
     return (
-      <div className="w-full overflow-x-auto space-x-0.5 flex p-1">
+      <div className="w-full overflow-x-auto space-x-2 flex p-1">
         <div className="flex space-x-0.5">
           <button
             onClick={() => onClick("ADMIN_ORGANIZATION")}
@@ -167,7 +167,7 @@ export const Filters = () => {
   }
 
   return (
-    <div className="w-full overflow-x-auto space-x-0.5 flex p-1">
+    <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <div className="flex space-x-0.5">
         <button
           onClick={() => onClick("All")}
