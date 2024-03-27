6b75c1b3786ffa177e86323e3ef5059feafc98ad
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-03-27 16:35:06 +0200

    Update share-ai form

--- components/share-ai-form.tsx
+++ components/share-ai-form.tsx
@@ -108,119 +108,118 @@ export const ShareAIForm = ({ ai, onSuccess }: ShareAIFormProps) => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <h3 className="text-lg font-medium">Share {ai.name}</h3>
           {ai.visibility === AIVisibility.PUBLIC ||
-          ai.visibility === AIVisibility.UNLISTED ? (
-            <div>
-              <div className="flex justify-between mb-2">
-                <div className="mb-4">
-                  <TwitterShareButton
-                    title={shareTitle}
-                    url={aiLink}
-                    hashtags={["appdirectai"]}
-                    related={["AppDirect"]}
-                    windowWidth={1100}
-                    windowHeight={700}
-                  >
-                    <XIcon size={32} round />
-                  </TwitterShareButton>
-                </div>
-                <div className="mb-4">
-                  <LinkedinShareButton url={aiLink}>
-                    <LinkedinIcon size={32} round />
-                  </LinkedinShareButton>
-                </div>
-                <div>
-                  <FacebookShareButton url={aiLink}>
-                    <FacebookIcon size={32} round />
-                  </FacebookShareButton>
-                  <div className="flex justify-center">
-                    <FacebookShareCount url={aiLink}>
-                      {(count) => count}
-                    </FacebookShareCount>
+            (ai.visibility === AIVisibility.UNLISTED && (
+              <div>
+                <div className="flex justify-between mb-2">
+                  <div className="mb-4">
+                    <TwitterShareButton
+                      title={shareTitle}
+                      url={aiLink}
+                      hashtags={["appdirectai"]}
+                      related={["AppDirect"]}
+                      windowWidth={1100}
+                      windowHeight={700}
+                    >
+                      <XIcon size={32} round />
+                    </TwitterShareButton>
+                  </div>
+                  <div className="mb-4">
+                    <LinkedinShareButton url={aiLink}>
+                      <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                  </div>
+                  <div>
+                    <FacebookShareButton url={aiLink}>
+                      <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                    <div className="flex justify-center">
+                      <FacebookShareCount url={aiLink}>
+                        {(count) => count}
+                      </FacebookShareCount>
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <EmailShareButton
+                      url={aiLink}
+                      subject={shareTitle}
+                      body="body"
+                    >
+                      <EmailIcon size={32} round />
+                    </EmailShareButton>
+                  </div>
+                  <div className="mb-4">
+                    <PocketShareButton url={aiLink} title={shareTitle}>
+                      <PocketIcon size={32} round />
+                    </PocketShareButton>
+                  </div>
+                  <div>
+                    <RedditShareButton
+                      url={aiLink}
+                      title={shareTitle}
+                      windowWidth={1100}
+                      windowHeight={700}
+                    >
+                      <RedditIcon size={32} round />
+                    </RedditShareButton>
+                    <div className="flex justify-center">
+                      <RedditShareCount url={aiLink} />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <TelegramShareButton url={aiLink} title={shareTitle}>
+                      <TelegramIcon size={32} round />
+                    </TelegramShareButton>
+                  </div>
+                  <div className="mb-4">
+                    <WhatsappShareButton
+                      url={aiLink}
+                      title={shareTitle}
+                      separator=":: "
+                      windowWidth={1100}
+                      windowHeight={700}
+                    >
+                      <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
                   </div>
                 </div>
-                <div className="mb-4">
-                  <EmailShareButton
-                    url={aiLink}
-                    subject={shareTitle}
-                    body="body"
+                <div className="flex items-center">
+                  <p className="text-xs px-3 py-3 mr-2 bg-ring/10 rounded-lg w-full">
+                    {aiLink}
+                  </p>
+                  <Button
+                    onClick={onCopy}
+                    size="icon"
+                    variant="ghost"
+                    type="button"
                   >
-                    <EmailIcon size={32} round />
-                  </EmailShareButton>
-                </div>
-                <div className="mb-4">
-                  <PocketShareButton url={aiLink} title={shareTitle}>
-                    <PocketIcon size={32} round />
-                  </PocketShareButton>
-                </div>
-                <div>
-                  <RedditShareButton
-                    url={aiLink}
-                    title={shareTitle}
-                    windowWidth={1100}
-                    windowHeight={700}
-                  >
-                    <RedditIcon size={32} round />
-                  </RedditShareButton>
-                  <div className="flex justify-center">
-                    <RedditShareCount url={aiLink} />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <TelegramShareButton url={aiLink} title={shareTitle}>
-                    <TelegramIcon size={32} round />
-                  </TelegramShareButton>
-                </div>
-                <div className="mb-4">
-                  <WhatsappShareButton
-                    url={aiLink}
-                    title={shareTitle}
-                    separator=":: "
-                    windowWidth={1100}
-                    windowHeight={700}
-                  >
-                    <WhatsappIcon size={32} round />
-                  </WhatsappShareButton>
+                    <Copy className="w-4 h-4" />
+                  </Button>
                 </div>
               </div>
-              <div className="flex items-center">
-                <p className="text-xs px-3 py-3 mr-2 bg-ring/10 rounded-lg w-full">
-                  {aiLink}
-                </p>
-                <Button
-                  onClick={onCopy}
-                  size="icon"
-                  variant="ghost"
-                  type="button"
-                >
-                  <Copy className="w-4 h-4" />
-                </Button>
-              </div>
+            ))}
+          <>
+            <FormField
+              name="teammates"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Share with teammates</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Ex: jennifer.wallace@acme.com, joe.hamm@acme.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="w-full flex flex-row-reverse">
+              <Button size="lg" variant="ring">
+                Share
+              </Button>
             </div>
-          ) : (
-            <>
-              <FormField
-                name="teammates"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Add teammates</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder="Ex: jennifer.wallace@acme.com, joe.hamm@acme.com"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="w-full flex flex-row-reverse">
-                <Button size="lg" variant="ring">
-                  Share
-                </Button>
-              </div>
-            </>
-          )}
+          </>
         </form>
       </Form>
     </div>
