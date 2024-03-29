21c24798b49ebd1a6ae40c477ad9822eae2cfc6b
vipuldunde <vipul.dunde@appdirect.com>
2024-03-29 20:30:14 +0530

    AIA-139 | Improve Category Access Level, Option Select, default value and updated options.

---
+++ components/category-types.ts
@@ -0,0 +1,11 @@
+export const CategoryTypes = [
+    { name: "AI Models", type: "AI_MODELS" },
+    { name: "Productivity", type: "PRODUCTIVITY" },
+    { name: "Learning & Development", type: "LEARNING_DEVELOPMENT" },
+    { name: "Marketing", type: "MARKETING" },
+    { name: "Sales", type: "SALES" },
+    { name: "Information Technology", type: "INFORMATION_TECHNOLOGY" },
+    { name: "Engineering", type: "ENGINEERING" },
+    { name: "Human Resources", type: "HUMAN_RESOURCES" },
+    { name: "Accounting & Finance", type: "ACCOUNTING_FINANCE" },
+];
