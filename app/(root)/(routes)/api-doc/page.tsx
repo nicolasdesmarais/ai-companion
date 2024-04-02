0b44555529a12dd0c040c89b8a195117821dfb53
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-04-02 18:33:17 +0300

    Remove redirect from api-doc page

--- app/(root)/(routes)/api-doc/page.tsx
+++ app/(root)/(routes)/api-doc/page.tsx
@@ -1,15 +1,7 @@
 import { PaywallBanner } from "@/components/paywall-banner";
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 import ReactSwagger from "./components/react-swagger";
 
 export default async function IndexPage() {
-  const { userId } = auth();
-
-  if (!userId) {
-    return redirect("/public/api-doc");
-  }
-
   return (
     <>
       <section className="container">
