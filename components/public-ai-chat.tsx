40496b8da9bc0c6fc2b585c089b7e0d8847794d4
Dmitry Torba <dmitrytorba@gmail.com>
2024-04-05 11:49:42 -0700

    fix(AIA-159) linkedin inspector fetch

--- components/public-ai-chat.tsx
+++ components/public-ai-chat.tsx
@@ -13,6 +13,15 @@ export const PublicAiChat = async ({ aiId }: Props) => {
   try {
     const ais = await aiService.findPublicAIs();
     const ai = await aiService.findPublicAIById(aiId);
+
+    const aiLink = `https://appdirect.ai/public/ai/${aiId}`;
+    // This is a workaround to fix the issue with LinkedIn not rendering preview
+    fetch(
+      `https://www.linkedin.com/post-inspector/inspect/${encodeURIComponent(
+        aiLink
+      )}`
+    );
+
     if (!ai) {
       throw new Error("AI not found");
     }
