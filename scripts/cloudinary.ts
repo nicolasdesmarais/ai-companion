7a72050e40a712fec37a301461d50a937af68a45
Dmitry Torba <dmitrytorba@gmail.com>
2024-04-15 14:52:35 -0700

    feat(cloudinary) migrate to new account

---
+++ scripts/cloudinary.ts
@@ -0,0 +1,35 @@
+const { PrismaClient } = require("@prisma/client");
+const cloudinary = require("cloudinary");
+
+const prismadb = new PrismaClient();
+
+async function main() {
+  try {
+    const ais = await prismadb.aI.findMany();
+    console.log(`Migrating images from ${ais.length} AIs ...`);
+    for (const ai of ais) {
+      if (ai.src) {
+        console.log(
+          `Migrating ${ai.name}'s image to the new cloudinary account...`
+        );
+        const data = await cloudinary.v2.uploader.unsigned_upload(
+          ai.src,
+          "bawmwjeq"
+        );
+        await prismadb.aI.update({
+          where: { id: ai.id },
+          data: {
+            src: data.secure_url,
+          },
+        });
+        console.log("Done.");
+      }
+    }
+  } catch (error) {
+    console.error("Error migrating cloudinary:", error);
+  } finally {
+    await prismadb.$disconnect();
+  }
+}
+
+main();
