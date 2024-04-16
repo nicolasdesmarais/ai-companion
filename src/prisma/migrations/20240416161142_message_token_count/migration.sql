0064f9e24644c868ea35c87dc42beb6528c286fe
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-04-16 19:30:55 +0300

    Populate token_count column

--- src/prisma/migrations/20240416161142_message_token_count/migration.sql
+++ src/prisma/migrations/20240416161142_message_token_count/migration.sql
@@ -1,2 +1,6 @@
 -- AlterTable
 ALTER TABLE `messages` ADD COLUMN `token_count` INTEGER NULL;
+
+-- Populate token_count
+UPDATE messages
+SET token_count = ifnull(json_extract(metadata, "$.tokensUsed"), 0) + ifnull(json_extract(metadata, "$.knowledgeTokensReturned"), 0);
