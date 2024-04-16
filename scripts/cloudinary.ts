a83f48ae75f337fd4c00edfe05933a038f896b3a
Dmitry Torba <dmitrytorba@gmail.com>
2024-04-16 07:29:49 -0700

    feat(cloudinary) error handle migration

--- scripts/cloudinary.ts
+++ scripts/cloudinary.ts
@@ -1,34 +1,38 @@
-import prismadb from "@/src/lib/prismadb";
-
+const Prisma = require("@prisma/client");
 const cloudinary = require("cloudinary");
+const prismadb = new Prisma.PrismaClient();
 
-async function main() {
+async function migrateCloudinary() {
   try {
     const ais = await prismadb.aI.findMany();
     console.log(`Migrating images from ${ais.length} AIs ...`);
     for (const ai of ais) {
-      if (ai.src) {
-        console.log(
-          `Migrating ${ai.name}'s image to the new cloudinary account...`
-        );
-        const data = await cloudinary.v2.uploader.unsigned_upload(
-          ai.src,
-          "bawmwjeq"
-        );
-        await prismadb.aI.update({
-          where: { id: ai.id },
-          data: {
-            src: data.secure_url,
-          },
-        });
-        console.log("Done.");
+      if (ai.src && ai.src.includes("dew7iqxa4")) {
+        try {
+          console.log(
+            `Migrating ${ai.name}'s image to the new cloudinary account...`
+          );
+          const data = await cloudinary.v2.uploader.unsigned_upload(
+            ai.src,
+            "bawmwjeq"
+          );
+          await prismadb.aI.update({
+            where: { id: ai.id },
+            data: {
+              src: data.secure_url,
+            },
+          });
+          console.log("Done.");
+        } catch (error) {
+          console.error("Error migrating cloudinary:", error);
+        }
       }
     }
   } catch (error) {
-    console.error("Error migrating cloudinary:", error);
+    console.error("Error:", error);
   } finally {
     await prismadb.$disconnect();
   }
 }
 
-main();
+migrateCloudinary();
