1658020eb802d646f9ca8ec801e42ce84a6db6a0
Fivos Constantinou <fivos.constantinou@appdirect.com>
2024-04-18 11:13:49 +0300

    Add rate limit for token usage (#352)
    * Add rate limit for token usage
    
    * Fix calculation of estimated token usage in knowledge callback
    
    * Move rate limit after fetching knowledge
    
    * Formatting

--- src/domain/services/ChatService.ts
+++ src/domain/services/ChatService.ts
@@ -16,18 +16,27 @@ import {
   ChatMessageDto,
 } from "@/src/domain/models/Chats";
 import prismadb from "@/src/lib/prismadb";
+import { tokenBucketRateLimit } from "@/src/lib/rate-limit";
 import { getTokenLength } from "@/src/lib/tokenCount";
 import { AuthorizationContext } from "@/src/security/models/AuthorizationContext";
 import { SystemMessage } from "@langchain/core/messages";
 import { Prisma, Role } from "@prisma/client";
 import { ChatSecurityService } from "../../security/services/ChatSecurityService";
-import { EntityNotFoundError, ForbiddenError } from "../errors/Errors";
+import {
+  EntityNotFoundError,
+  ForbiddenError,
+  RateLimitError,
+} from "../errors/Errors";
 import { ChatRepository } from "../ports/outgoing/ChatRepository";
 import aiModelService from "./AIModelService";
 import aiService from "./AIService";
 import knowledgeService from "./KnowledgeService";
 
 const BUFFER_TOKENS = 200;
+
+const TOKEN_BUCKET_REFILL_RATE = 700;
+const TOKEN_BUCKET_INTERVAL = "1 m";
+const TOKEN_BUCKET_MAX_TOKENS = 1000000;
 
 const listChatsResponseSelect: Prisma.ChatSelect = {
   id: true,
@@ -405,7 +414,8 @@ export class ChatService {
     const llmTime = Math.round(end - endKnowledge);
     const totalTime = Math.round(end - start);
 
-    const tokenCount = recordedTokensUsed + knowledgeTokensReturned;
+    const tokenCount =
+      Number(recordedTokensUsed) + Number(knowledgeTokensReturned);
 
     const message: ChatMessageDto = {
       role: Role.system,
@@ -479,6 +489,25 @@ export class ChatService {
     console.log(
       `chatId: ${chat.id}, knowledgeTokensReturned: ${vectorKnowledge.tokensReturned}`
     );
+
+    const identifier = "chat-" + chat.orgId;
+    const tokenUsage =
+      Number(tokensUsed) +
+      Number(answerTokens) +
+      Number(vectorKnowledge.tokensReturned);
+    const isWithinRateLimit = await tokenBucketRateLimit(
+      identifier,
+      TOKEN_BUCKET_REFILL_RATE,
+      TOKEN_BUCKET_INTERVAL,
+      TOKEN_BUCKET_MAX_TOKENS,
+      tokenUsage
+    );
+    if (!isWithinRateLimit) {
+      throw new RateLimitError(
+        `Token usage rate limit exceeded for orgId=${chat.orgId}`
+      );
+    }
+
     return vectorKnowledge;
   }
 
--- src/lib/rate-limit.ts
+++ src/lib/rate-limit.ts
@@ -2,6 +2,9 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { NextRequest } from "next/server";
 
+type Unit = "ms" | "s" | "m" | "h" | "d";
+type Duration = `${number} ${Unit}` | `${number}${Unit}`;
+
 export async function rateLimit(identifier: string) {
   const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
@@ -11,6 +14,26 @@ export async function rateLimit(identifier: string) {
   });
 
   return await ratelimit.limit(identifier);
+}
+
+export async function tokenBucketRateLimit(
+  identifier: string,
+  refillRate: number,
+  interval: Duration,
+  maxTokens: number,
+  requestedTokens: number
+): Promise<boolean> {
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.tokenBucket(refillRate, interval, maxTokens),
+    analytics: true,
+    prefix: "@upstash/ratelimit",
+  });
+
+  const { success } = await ratelimit.limit(identifier, {
+    rate: requestedTokens,
+  });
+  return success;
 }
 
 export async function rateLimitRequest(req: NextRequest) {
